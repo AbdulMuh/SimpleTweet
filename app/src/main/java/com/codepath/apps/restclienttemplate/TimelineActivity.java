21ea4ec4156d5d0a445d4a13ee829a0259a45340
Abdul Muhammad <abdulmuhammad331@gmail.com>
2021-02-26 17:33:26 -0800

    Fixed refresh bug

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -45,7 +45,7 @@ public class TimelineActivity extends AppCompatActivity {
             @Override
             public void onRefresh() {
                 Log.i(TAG,"fetching new data!");
-
+                populateHomeTimeline();
             }
         });
 
